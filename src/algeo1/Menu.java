0d8c789bab4ade74acc98dda9d0cae9a3bb4297c
Irfan <119424337+SipalingCoding@users.noreply.github.com>
2023-09-22 08:04:20 -0700

    Update Menu.java

--- src/algeo1/Menu.java
+++ src/algeo1/Menu.java
@@ -3,12 +3,12 @@ package algeo1;
 import java.util.Scanner;
 
 public class Menu {
-	
+
 	public static void menuSPL() {
 		int submenu;
-		
+
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("MENU\n"
 				+ "1. Metode eliminasi Gauss\n"
 				+ "2. Metode eliminasi Gauss-Jordan\n"
@@ -22,12 +22,35 @@ public class Menu {
 			daftarMenu();
 		}
 	}
-	
+
+	public static int menuInput() {
+		int submenu;
+
+		Scanner s = new Scanner(System.in);
+		System.out.println("\n1. Input dari Keyboard");
+		System.out.println("2. Input dari File\n==============");
+		System.out.print("Masukan: ");
+
+		submenu = s.nextInt();
+
+		return submenu;
+
+	}
+
+	public static String inputFile() {
+		Scanner s = new Scanner(System.in);
+		System.out.print("\nMasukkan nama file (.txt) (Cth : a1.txt) : ");
+
+		String submenu = s.nextLine();
+
+		return submenu;
+	}
+
 	public static void menuDeterminan() {
 		int submenu;
-		
+
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("MENU\n"
 				+ "1. Metode reduksi baris\n"
 				+ "2. Metode ekspansi kofaktor\n"
@@ -39,12 +62,12 @@ public class Menu {
 			daftarMenu();
 		}
 	}
-	
+
 	public static void menuInverse() {
 		int submenu;
-		
+
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("MENU\n"
 				+ "1. Metode eliminiasi Gauss-Jordan\n"
 				+ "2. Metode adjoint\n"
@@ -56,12 +79,12 @@ public class Menu {
 			daftarMenu();
 		}
 	}
-	
+
 	public static void daftarMenu() {
 		int menu;
-		
+
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("MENU");
 		System.out.println("1. Sistem Persamaaan Linier\n"
 				+ "2. Determinan\n"
@@ -70,31 +93,59 @@ public class Menu {
 				+ "5. Interpolasi Bicubic Spline\n"
 				+ "6. Regresi linier berganda\n"
 				+ "7. Keluar");
-		
+
 		System.out.print("Pilihan menu: ");
 		menu = s.nextInt();
 		System.out.println();
-		
+
 		while (menu < 1 || menu > 7) {
 			System.out.println("Input tidak valid.");
 			System.out.print("Pilihan menu: ");
 			menu = s.nextInt();
 		}
-		
+
 		switch (menu) {
-		case 1:
-			menuSPL();
-			break;
-		case 2:
-			menuDeterminan();
-			break;
-		case 3:
-			menuInverse();
-			break;
-		case 7:
-			System.exit(0);
-			break;
+			case 1:
+				menuSPL();
+				int a = menuInput();
+				if (a == 1) {
+					// Scanner userInput = new Scanner(System.in);
+					// userInput = nextInt();
+					System.out.println("untuk Mengisi SPL Matriks");
+				} else if (a == 2) {
+					inputFile();
+
+				}
+
+				break;
+			case 2:
+				menuDeterminan();
+				int b = menuInput();
+				if (b == 1) {
+					// Scanner userInput = new Scanner(System.in);
+					// userInput = nextInt();
+					System.out.println("untuk Mengisi Determinan");
+				} else if (b == 2) {
+					inputFile();
+
+				}
+				break;
+			case 3:
+				menuInverse();
+				int c = menuInput();
+				if (c == 1) {
+					// Scanner userInput = new Scanner(System.in);
+					// userInput = nextInt();
+					System.out.println("untuk Mengisi Invers");
+				} else if (c == 2) {
+					inputFile();
+
+				}
+				break;
+			case 7:
+				System.exit(0);
+				break;
 		}
-		
+
 	}
 }
